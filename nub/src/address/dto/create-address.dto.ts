3e481f0342b80cf039ecb7fc3871d87ce4a20d30
Gniper <111673936+TranquilGnyp3rz@users.noreply.github.com>
2024-08-16 15:19:05 +0000

    Add CreateAddressDto and Address Entity

--- nub/src/address/dto/create-address.dto.ts
+++ nub/src/address/dto/create-address.dto.ts
@@ -1,1 +1,39 @@
-export class CreateAddressDto {}
+import { IsString, IsNotEmpty, IsNumber, IsOptional } from "class-validator";
+
+export class CreateAddressDto {
+    @IsString()
+    @IsNotEmpty()
+    name: string;
+
+    @IsString()
+    @IsNotEmpty()
+    street: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    city: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    state: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    country: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @IsOptional()
+    postalCode: string;
+
+    @IsNumber()
+    @IsOptional()
+    latitude: number;
+
+    @IsNumber()
+    @IsOptional()
+    longitude: number;
+}
--- nub/src/address/entities/address.entity.ts
+++ nub/src/address/entities/address.entity.ts
@@ -1,1 +1,23 @@
-export class Address {}
+import { Exclude } from 'class-transformer';
+
+export class Address {
+  constructor(
+    public id: string,
+    public name: string,
+    public street: string,
+    public city: string,
+    public state: string,
+    public country: string,
+    public postalCode: string,
+    public latitude: number,
+    public longitude: number,
+    public userId?: string,
+    public restaurantId?: string,
+  ) {}
+
+  @Exclude()
+  readonly createdAt: Date = new Date();
+
+  @Exclude()
+  readonly updatedAt: Date = new Date();
+}
