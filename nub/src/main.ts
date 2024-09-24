93900dbaf4e602ad423ef5ca486f12ae67ae5a20
TranquilGnyp3rz <gnipe@proton.me>
2024-09-24 20:39:11 +0100

    Modify menu.service

--- nub/src/main.ts
+++ nub/src/main.ts
@@ -1,13 +1,14 @@
-import { NestFactory, Reflector } from '@nestjs/core';
+import { HttpAdapterHost, NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
+import { HttpStatus, ValidationPipe } from '@nestjs/common';
 import { SupabaseAuthGuard } from 'src/auth/guards/supa.guard';
 import { RolesGuard } from './auth/guards/role.guard';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { PrismaClientExceptionFilter } from 'nestjs-prisma';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
   app.useGlobalGuards(new SupabaseAuthGuard());
   app.useGlobalGuards(new RolesGuard(new Reflector()));
 
@@ -19,6 +20,16 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true,
+  }));
+
+  const { httpAdapter } = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new PrismaClientExceptionFilter(httpAdapter, {
+    P2000: HttpStatus.BAD_REQUEST,
+    P2002: HttpStatus.CONFLICT,
+    P2025: HttpStatus.NOT_FOUND,
+  }));
 
   await app.listen(3000);
 }
--- nub/src/menu/menu.service.ts
+++ nub/src/menu/menu.service.ts
@@ -1,31 +1,12 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { CreateMenuDto } from './dto/create-menu.dto';
 import { UpdateMenuDto } from './dto/update-menu.dto';
 import { PrismaService } from 'nestjs-prisma';
 
-
-/**
- * 
-model Dish {
-  id           String      @id @default(uuid())
-  name         String
-  description  String
-  price        Float
-  rating       Float
-  pictures     String[]
-  restaurantId String
-  restaurant   Restaurant  @relation(fields: [restaurantId], references: [id])
-  orderItems   OrderItem[]
-  createdAt    DateTime    @default(now())
-  updatedAt    DateTime    @updatedAt
-  reviews      Review[]
-  @@map("dishes")
-}
- */
 @Injectable()
 export class MenuService {
   constructor(private readonly prismaService: PrismaService) {}
-  
+
   async create(createMenuDto: CreateMenuDto) {
     return await this.prismaService.dish.create({
       data: {
@@ -39,7 +20,6 @@ export class MenuService {
             id: createMenuDto.restaurantId,
           },
         },
-
       },
       select: {
         id: true,
@@ -71,7 +51,7 @@ export class MenuService {
   }
 
   async findOne(id: string) {
-    return await this.prismaService.dish.findUnique({
+    const dish = await this.prismaService.dish.findUnique({
       where: {
         id: id,
       },
@@ -85,6 +65,10 @@ export class MenuService {
         restaurant: true,
       },
     });
+    if (!dish) {
+      throw new HttpException('Dish not found', 404);
+    }
+    return dish;
   }
 
   async update(id: string, updateMenuDto: UpdateMenuDto) {
